a5ea984c0561e01e0205ac72f7326e6b98978362
Dame <camaradame@gmail.com>
2018-09-24 13:17:45 +0000

    first commit

---
+++ src/providers/gloabal-variable/gloabal-variable.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class  GlobalVariableProvider {
+/*  public URL1 ="http://services.ajit.sn/ws/parti/";
+  public URL ="http://ajit.sn/parti/ws/";*/
+  public URL ="http://ajit.sn/parrainage/ws/";
+  public Ageminimum = 18;
+  mode="internet";
+
+}
